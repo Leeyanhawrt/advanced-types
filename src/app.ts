0df4f950ff39ee538aa96c9225de8be3ad72410d
Leeyanhawrt <leeyan1108@gmail.com>
2023-08-16 14:34:55 -0400

    Learn about discriminated unions

--- src/app.ts
+++ src/app.ts
@@ -79,3 +79,30 @@ const useVehicle = (vehicle: Vehicle) => {
 
 useVehicle(v1);
 useVehicle(v2);
+
+interface Bird {
+  type: "bird";
+  flyingSpeed: number;
+}
+
+interface Horse {
+  type: "horse";
+  runningSpeed: number;
+}
+
+type Animal = Bird | Horse;
+
+const moveAnimal = (animal: Animal) => {
+  let speed;
+  switch (animal.type) {
+    case "bird":
+      speed = animal.flyingSpeed;
+      break;
+    case "horse":
+      speed = animal.runningSpeed;
+      break;
+  }
+  console.log(`Moving at speed: ${speed}`);
+};
+
+moveAnimal({ type: "bird", flyingSpeed: 5 });
